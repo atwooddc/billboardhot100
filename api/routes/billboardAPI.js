af750ed5cb38a1c7bf44ab23d87a6e67a36709d1
atwooddc <david.c.atwood@vanderbilt.edu>
2021-12-02 17:22:51 -0600

    updating web scraping to maintain compatibility

--- api/routes/billboardAPI.js
+++ api/routes/billboardAPI.js
@@ -10,17 +10,34 @@ router.get("/",function(req,res,next){
     request('https://www.billboard.com/charts/hot-100', function (error, response, html) {
         if (!error && response.statusCode == 200) {
             const $ = cheerio.load(html);
-            const listItems = $(".chart-element__information");
+            const listItems = $(".o-chart-results-list-row-container");
             const songs = [];
             listItems.each((idx, el) => {
-                const title = $(el).children(".chart-element__information__song").text();
-                const artist = $(el).children(".chart-element__information__artist").text();
-                const titleAndArtist = "\"" + title + "\"" + " by " + artist;
+                const title = $(el).children("h3").text();
+                // const artist = $(el).children("").text();
+                const titleAndArtist = "\"" + title + "\"" + " by "; // + artist;
                 songs.push(titleAndArtist);
             });
             res.send(songs);
         }
     });
 });
+
+// router.get("/",function(req,res,next){
+//     request('https://www.billboard.com/charts/hot-100', function (error, response, html) {
+//         if (!error && response.statusCode == 200) {
+//             const $ = cheerio.load(html);
+//             const listItems = $(".chart-element__information");
+//             const songs = [];
+//             listItems.each((idx, el) => {
+//                 const title = $(el).children(".chart-element__information__song").text();
+//                 const artist = $(el).children(".chart-element__information__artist").text();
+//                 const titleAndArtist = "\"" + title + "\"" + " by " + artist;
+//                 songs.push(titleAndArtist);
+//             });
+//             res.send(songs);
+//         }
+//     });
+// });
 
 module.exports=router;
